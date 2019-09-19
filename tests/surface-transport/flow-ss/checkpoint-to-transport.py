5c7a8b912d1069d49ecb2170c6e64ebac7764407
Ahmad Jan <ajkhattak@gmail.com>
2019-09-19 12:11:59 -0400

    adding inputfiles for v0.87 and v0.88, working and results consistent with Scott's data.

--- tests/surface-transport/flow-ss/checkpoint-to-transport.py
+++ tests/surface-transport/flow-ss/checkpoint-to-transport.py
@@ -1,12 +1,32 @@
 import h5py
 import numpy as np
-d = h5py.File("checkpoint00025.h5",'r')
+m_rho = 55000./55000.
+edge_l = 6.87
+d = h5py.File("checkpoint00237.h5",'r')
 d2 = h5py.File("ss_flow_results.h5",'w')
 d2.create_dataset("time", data=np.array([0.,]))
 flux = d2.create_group("surface-mass_flux.face.0")
 pd = d2.create_group("surface-ponded_depth.cell.0")
-flux.create_dataset("0", data=d['surface-mass_flux.face.0'][:])
-pd.create_dataset("0", data=d['surface-ponded_depth.cell.0'][:])
+
+print (d2.keys())
+mass_flux = d['surface-mass_flux.face.0'][:]*m_rho
+#m = 0.08*6.87*0.183
+for i in range(len(mass_flux)):
+	if mass_flux[i] > 0:
+		mass_flux[i] = 0.1
+	elif mass_flux[i] < 0:
+		mass_flux[i] = -0.1
+	else:
+		mass_flux[i] = 0
+mass_flux = [round(x,10) for y in mass_flux for x in y]
+
+print d['surface-mass_flux.face.0'][:]
+print mass_flux
+
+p_depth = d['surface-ponded_depth.cell.0'][:]
+flux.create_dataset("0",data=mass_flux)
+pd.create_dataset("0",data=p_depth)
+print (p_depth)
 
 d.close()
 d2.close()
